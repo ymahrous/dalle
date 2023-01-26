9c292cb1701436ec4a1527c78fb4c5f7d8365abb
Yousef Mahrous <yousefmahrous@YM-MBP.local>
2023-01-26 18:46:58 +0200

    commit

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -14,7 +14,7 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
         const aiResponse = await openai.createImage({
-            prompt: prompt,
+            prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -26,7 +26,7 @@ router.route('/').post(async (req, res) => {
     try {
         const { name, prompt, photo } = req.body;
         const photoUrl = await cloudinary.uploader.upload(photo);
-        const newPost = await Post({
+        const newPost = await Post.create({
             name,
             prompt,
             photo: photoUrl.url,
